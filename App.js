5fd614d59b91e7bebf136ddbbc66c4e4579e41fc
Atul <atulreso1@gmail.com>
2023-07-09 02:47:39 +0530

    laying the foundation

--- App.js
+++ App.js
@@ -1,54 +1,73 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/*
-Parcel do:
-
-* Created a server (parcel-cache)
-* HMR => Hot Module Replacement
-* File Watcher algorithm - C++  ==> Parcel watches every file in your project root (including node_modules)
-* Bundling
-* MINIFY the files (Minification)
-* Compressing
-* Cleaning our Code
-* Dev and Production Build
-* Super Fast build algorithm
-* Image optimization
-* Caching while development (Reliable caching)
-* Compatible with older versions of browser
-* HTTPS on dev env -> npx parcel index.html --https
-* parcel manage port number also
-* Consistent Hashing Algorithm todo all bundling
-* Code splitting
-* Differential Bundling -> it give the support to older browsers
-* Parcel is a Zero config bundler
-* Tree-shaking => Removing un-wanted code
-* Diagnostic -> show error in beautiful way
-* Good Error Handling
-
-* Transitive Dependencies:  We've package manager which handles and take care of Transitive Dependencies
-
-POWER -> PM -> Home Minister -> CMs -> cabinet minister -> minister -> Parliament  ==> PUBLIC
-Apps -> React -> Parcel/Webpack (Bundler) -> node manager (node_modules) -> dependencies -> devDependencies ==> Transitive- Dependencies
-
--> ** above all thing done in .parcel-cache
-
-do not put package-lock.json files in gitignore
-Always put parcel-cache & node_module in gitignore
-
-How to know which file I put in gitignore?
-
-Anything which is auto generated on server in your project put in gitignore except package-lock.json
-
-
-to run parcel -> npx parcel entry-file(index.js/App.js in my case entry file is index.html)
-to build => npx parcel build index.html (for this command we don't need "main": "App.js" in package.json file also no need dist folder, this command will automatically create minify build for out apps)
-
-https://browserslist.dev
-*/
+//  React.createElement => JS Object =>  HTMLElement(render)
 
 const heading = React.createElement("h1", {}, "hay ignitions 🚀");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-const root = ReactDOM.createRoot(document.querySelector("#root")); // this will also work
-console.log({ heading, root });
-root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// react and jsx both are different
+//  jsx is not a html, jsx different from html. JSX is a HTML-like or XML-like syntax
+//  JSX (transpiled before it reaches the JS Engine) - PARCEL - BABEL
+//  BABEL- It is transpiler/JS-compiler that convert your JSX to browser can understand.
+
+//  How the JSX run under the hood in react?
+//  JSX converted to React.createElement then it makes JS element object then that js element  rendered in DOM as HTMLElement
+
+//  JSX => Babel transpile it to React.createElement => ReactElement-JS Object =>  HTMLElement(render)
+
+//  for single line jsx
+// const jsxHeading = <h2 id="jsxId">jsx heading</h2>;
+const jsxHeading = <h2 className="jsxId">jsx heading start</h2>;
+
+//  for multiline jsx, because babel want to know where jsx start and  where it is going to end
+const multiJsx = (
+  <>
+    <h1> multiline jsx</h1>
+    <h4>check line</h4>
+    <h2 className="jsxIdM" tabIndex="1">
+      jsx heading
+    </h2>
+    {jsxHeading}
+  </>
+);
+console.log(heading, root, jsxHeading);
+
+// React Component -> Functional component & Class based component
+// React Functional Component -> It is normal javascript function which return some peace of jsx, make sure function component always start from capital latter.
+
+const fn = () => {
+  return true;
+};
+
+const fn2 = () => true;
+//  Above fn & fn2 are equivalent.
+
+const TestFunctionalComponent = () => {
+  return <h1 className="jsxFunc">React Functional Components1</h1>;
+};
+
+//  this is also perfectly valid function component
+
+//  component composition => composing two component into one component.
+const TestFunctionalComponent2 = () => (
+  <>
+    <>
+      <div className="container">{multiJsx}</div>
+      <div className="container2">
+        {TestFunctionalComponent()}
+        <TestFunctionalComponent />
+        <TestFunctionalComponent></TestFunctionalComponent>
+      </div>
+      <h1 className="jsxFunc">React Functional Component2</h1>
+    </>
+  </>
+);
+
+// if we use multiple root.render then it don't show any error but it only render last root.render function
+
+
+
+// root.render(jsxHeading);
+// root.render(TestFunctionalComponent2());
+root.render(<TestFunctionalComponent/>);
